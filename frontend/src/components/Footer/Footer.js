769d00329ba6b734a2aa73450b384e93f32f08e4
RodrigoCoronelLoza <drigo31416@gmail.com>
2023-10-26 12:38:30 -0400

    header and footer

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.container}>
+        <p>Author: Rodrigo Coronel Loza</p>
+        <p>
+          <a href="mailto:drigo31416@gmail.com">drigo31416@gmail.com</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,58 @@
+import React from "react";
+import classes from "./header.module.css";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  const user = {
+    name: "Jhon",
+  };
+  const cart = {
+    totalCount: 10,
+  };
+
+  const logout = () => {};
+  return (
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <Link to="/" className={classes.logo}>
+          MUSIC STORE
+        </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to="">Quienes Somos</Link>
+            </li>
+
+            <li>
+              <Link to="/">Woodwinds</Link>
+            </li>
+
+            <li>
+              <Link to="/">Brass</Link>
+            </li>
+            {user ? (
+              <li className={classes.menu_container}>
+                <Link to="/profile">{user.name}</Link>
+                <div className={classes.menu}>
+                  <Link to="/profile">Profile</Link>
+                  <Link to="/orders">Orders</Link>
+                  <a onClick={logout}>Logout</a>
+                </div>
+              </li>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+            <li>
+              <Link to="/cart">
+                Cart
+                {cart.totalCount > 0 && (
+                  <span className={classes.cart_count}>{cart.totalCount}</span>
+                )}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
